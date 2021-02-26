1390ec52c4a5f91896cfaeb43d1031bb970ca9df
Eden Ernandes <eden.santana@neurotech.com.br>
2021-02-26 01:15:28 -0300

    Add data normalization step

--- knn/test.py
+++ knn/test.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import KFold
 from knn import KNN
 import numpy as np
+from sklearn import preprocessing
 
 #load CM1
 data = arff.loadarff('./datasets/CM1.arff')
@@ -11,6 +12,14 @@ data = arff.loadarff('./datasets/CM1.arff')
 X = pd.DataFrame(data[0])
 Y = X['defects']
 X = X.drop(columns=['defects'])
+
+
+# normalize data
+arr = X.values
+scaler = preprocessing.MinMaxScaler()
+X_scaled = scaler.fit_transform(arr)
+X = pd.DataFrame(X_scaled)
+
 
 # create k-fold splits
 kf = KFold(n_splits=10)
@@ -24,8 +33,7 @@ for train, test in kf.split(X):
 
     clf.fit(X.iloc[train], Y.iloc[train])
     predictions = clf.predict(X.iloc[test])
-
-
+    
     acc = (np.sum(predictions == Y.iloc[test]) / len(test)) * 100
     print("Accuracy for fold {}: {}%".format(index, acc))
     index += 1
